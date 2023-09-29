05d2ebeb95e0cea3e86c9471fbadb72b99b38e35
Jumadi <54573301+jumadi-cloud@users.noreply.github.com>
2023-09-30 00:17:56 +0700

    Update app.py

--- app.py
+++ app.py
@@ -55,7 +55,7 @@ profile_pic = Image.open(profile_pic)
 # --- Bagian pengaturan foto profile ---
 col1, col2 = st.columns(2, gap="small")
 with col1:
-    st.image(profile_pic, width=260)
+    st.image(profile_pic, width=265)
     st.write("Programmer | Artificial Intelligence | Content Creator at Kelas Awan Pintar")
 
 with col2:
