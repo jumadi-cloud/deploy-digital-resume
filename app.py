b3c1e5a42604ef51b0e84d6edfeecda7d32aba70
Jumadi <54573301+jumadi-cloud@users.noreply.github.com>
2023-10-07 15:48:00 +0700

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from PIL import Image
 # --- PATH SETTINGS ---
 current_dir = Path(__file__).parent if "__file__" in locals() else Path.cwd()
 css_file = current_dir / "styles" / "main.css"
-resume_file = current_dir / "doc" / "CV-Jumadi"
+resume_file = current_dir / "doc" / "CV-Jumadi.pdf"
 profile_pic = current_dir / "doc" / "jumadi.png"
 
 
